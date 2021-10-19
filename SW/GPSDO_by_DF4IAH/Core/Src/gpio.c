507f77a5a6045b7c9a92e429c9897d1bbf3b6792
Ulrich Habel <espero7757@gmx.net>
2021-10-20 01:03:37 +0200

    SW - remove pull-down at hold relay output

--- SW/GPSDO_by_DF4IAH/Core/Src/gpio.c
+++ SW/GPSDO_by_DF4IAH/Core/Src/gpio.c
@@ -94,7 +94,7 @@ void MX_GPIO_Init(void)
   /*Configure GPIO pin : PtPin */
   GPIO_InitStruct.Pin = D12_HoRelay_GPIO_O_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(D12_HoRelay_GPIO_O_GPIO_Port, &GPIO_InitStruct);
 
